705c84ec9b6f05ec6561d3ebd17679c7fb1990ff
Roman <46465589+C0d3ing@users.noreply.github.com>
2025-12-04 05:30:34 -0800

    LFO bit truncated from 16 to 14 bits

--- FPGA/LFO/Src/LFO-LUTs/LUTgen.m
+++ FPGA/LFO/Src/LFO-LUTs/LUTgen.m
@@ -1,6 +1,6 @@
 %% Configuration
-res = 50;               % Number of samples between the range [sin(0), sin(2*pi)) 
-width = 16;             % Bit width (Must be >= 14 for amplitude 4410)
+res = 256;               % Number of samples between the range [sin(0), sin(2*pi)) 
+width = 14;             % Bit width (Must be >= 14 for amplitude 4410)
 amplitude = 4410;       % Peak amplitude (maximum delay)
 filename = 'sine_fixed.mem'; 
 
